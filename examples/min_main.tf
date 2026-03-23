baf4db29b185cf94d7c68171cb5ed829fda7c4c8
mima <m.mattes@telekom.de>
2026-03-23 14:24:38 +0100

    terrascan linting

--- examples/min_main.tf
+++ examples/min_main.tf
@@ -1,6 +1,7 @@
 module "base" {
   source = "registry.terraform.io/telekom-mms/base/azurerm"
 
+  # ts:skip=AC_AZURE_0389 Resource Lock is not managed by this module
   resource_group = {
     rg-mms-github = {
       location = "westeurope"
